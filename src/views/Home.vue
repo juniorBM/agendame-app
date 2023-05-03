02da116ed5334acbd336157cc1240956bc47ee03
juniorBM <juniorbev.mendonca@gmail.com>
2023-05-03 14:07:34 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <VContainer>
+    <VBtn @click="sanctum">Sanctum</VBtn>
+  </VContainer>
+</template>
+
+<script setup>
+  import axios from 'axios';
+  axios.defaults.withCredentials = true;
+  function sanctum() {
+    axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+      // Login...
+      console.log(response);
+    });
+  }
+</script>
